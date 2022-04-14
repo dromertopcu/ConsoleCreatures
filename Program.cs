03aaf4f0c59307d9506bf5144eae7ba8363fcb97
ethumana <omertopcu@me.com>
2022-04-14 17:40:58 +0300

    files added

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // Creating Console Creatures
+      Console.WriteLine(" .-.");  
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| 0 |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("Dark Monster\n\n---\n");
+      Console.WriteLine("c_     ___\n　 \\\\__|__|__\n　　 \\( o_o)\n　　　 > ~  >\n　　　/ 　 / \\\\\n　　 /　　/　 \\\\\n　　 \\　 )　　c/\n　　/　 /\n　 /　/|\n　(　( \\\n　|　|  \\\n　| / \\  )\n　| |　) |\n /  )(_/\n(_ /");
+      Console.WriteLine("Dancing Man");
+     }
+  }
+}
